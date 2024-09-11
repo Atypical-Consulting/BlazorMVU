0ea26adfb3255bf3e05cc3538a0061b02b3df800
Philippe Matray <phmatray@gmail.com>
2024-09-11 23:46:54 +0200

    feat: build with Nuke

---
+++ build/Build.cs
@@ -0,0 +1,98 @@
+using System;
+using System.Linq;
+using Nuke.Common;
+using Nuke.Common.CI;
+using Nuke.Common.Execution;
+using Nuke.Common.IO;
+using Nuke.Common.ProjectModel;
+using Nuke.Common.Tooling;
+using Nuke.Common.Tools.DotNet;
+using Nuke.Common.Tools.GitVersion;
+using Nuke.Common.Tools.MSBuild;
+using Nuke.Common.Utilities.Collections;
+using Serilog;
+using static Nuke.Common.EnvironmentInfo;
+using static Nuke.Common.IO.FileSystemTasks;
+using static Nuke.Common.IO.PathConstruction;
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
+
+class Build : NukeBuild
+{
+    /// Support plugins are available for:
+    ///   - JetBrains ReSharper        https://nuke.build/resharper
+    ///   - JetBrains Rider            https://nuke.build/rider
+    ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
+    ///   - Microsoft VSCode           https://nuke.build/vscode
+
+    public static int Main () => Execute<Build>(x => x.Compile);
+
+    [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
+    readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
+    
+    [Solution] readonly Solution Solution;
+    [GitVersion] readonly GitVersion GitVersion;
+    
+    AbsolutePath SourceDirectory => RootDirectory / "src";
+
+    Target Print => _ => _
+        .Executes(() =>
+        {
+            Log.Information("GitVersion = {Value}", GitVersion.MajorMinorPatch);
+        });
+    
+    Target Clean => _ => _
+        .Executes(() =>
+        {
+            Log.Information("Cleaning the output directory...");
+            
+            SourceDirectory
+                .GlobDirectories("**/bin", "**/obj")
+                .ForEach(x =>
+                {
+                    Log.Information($"Deleting {x}...");
+                    x.DeleteDirectory();
+                });
+        });
+
+    Target Restore => _ => _
+        .DependsOn(Clean)
+        .Executes(() =>
+        {
+            Log.Information("Restoring the solution...");
+            DotNetRestore(s => s
+                .SetProjectFile(Solution)
+                .SetVerbosity(DotNetVerbosity.normal));
+        });
+
+    Target Compile => _ => _
+        .DependsOn(Restore)
+        .Executes(() =>
+        {
+            Log.Information($"Compiling the solution with version {GitVersion.SemVer}...");
+            
+            DotNetBuild(s => s
+                .SetProjectFile(Solution)
+                .SetConfiguration(Configuration)
+                .SetAssemblyVersion(GitVersion.AssemblySemVer)
+                .SetFileVersion(GitVersion.AssemblySemFileVer)
+                .SetInformationalVersion(GitVersion.InformationalVersion)
+                .EnableNoRestore());
+        });
+    
+    Target Test => _ => _
+        .DependsOn(Compile)
+        .Executes(() =>
+        {
+            Log.Information("Testing the solution...");
+            
+            foreach (var project in Solution.AllProjects.Where(p => p.Name.EndsWith(".Tests")))
+            {
+                DotNetTest(x => x
+                    .SetProjectFile(project)
+                    .EnableNoRestore()
+                    .EnableNoBuild()
+                    .SetConfiguration(Configuration)
+                );
+            }
+        });
+}
