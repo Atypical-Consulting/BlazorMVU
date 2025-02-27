cd9617038d27b287163f129442405f8cb73b6fdc
Philippe Matray <phmatray@gmail.com>
2025-02-27 19:32:11 +0100

    test: add tests for all mvu components

---
+++ src/BlazorMVU.Tests/Components/MvuFetchRandomErrorTests.cs
@@ -0,0 +1,34 @@
+using BlazorMVU.Demo.Components;
+using BlazorMVU.Demo.Shared;
+
+namespace BlazorMVU.Tests.Components;
+
+public class MvuFetchRandomErrorTests : TestContext
+{
+    [Fact]
+    public async Task MvuFetchRandomError_Success()
+    {
+        // Arrange
+        var cut = RenderComponent<MvuFetchRandomError>();
+
+        // Wait a bit for the async method to finish
+        await Task.Delay(10);
+
+        // Assert
+        cut.Instance.State
+            .ButtonLabel.ShouldBe("Fetch Weather");
+    }
+
+    [Fact]
+    public Task MvuFetchRandomError_Loading()
+    {
+        // Arrange
+        var cut = RenderComponent<MvuFetchRandomError>();
+
+        // Assert
+        cut.Instance.State
+            .ButtonLabel.ShouldBe("Fetch Weather");
+
+        return Task.CompletedTask;
+    }
+}
